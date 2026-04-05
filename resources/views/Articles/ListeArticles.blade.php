5eab679c5d2bb157219e3e4c0ce23980669a374b
karim <karimseghir96@gmail.com>
2026-04-05 14:15:36 +0100

    Ajout de la fonction X

--- resources/views/Articles/ListeArticles.blade.php
+++ resources/views/Articles/ListeArticles.blade.php
@@ -14,7 +14,7 @@
 
 <body>
 
-    <a href="{{route("Articles.create")}}" class="ButtonAddArticle">Ajouter nouveau Article</a>    
+    <a href="{{route("Articles.create")}}" class="ButtonAddArticle">Ajouter nouveau Article finale</a>    
 
     <table>
         <tr>  
