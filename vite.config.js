884d9f3d5e47c0d37fae4f35b00353016a55f258
karim <karimseghir96@gmail.com>
2026-04-05 15:15:28 +0100

    Ajout de la fonction X

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,10 @@ export default defineConfig({
    
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js',
+                   'resources/css/Articles/ListeArticles.css',
+                    'resources/js/Articles/ListeArticles.js'
+            ],
             refresh: true,
         }),
         tailwindcss(),
